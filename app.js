730ed7b36ab1299b28da173eb21b4d7250c03c6b
bakirmohammed <bakirmohammed018@gmail.com>
2024-08-08 10:56:59 +0100

    create modification hello message

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+function handleClick() {
+    let h1 = document.getElementById('h');
+    if (h1.style.display == 'none') {
+        h1.style.display = 'block';
+    } else {
+        h1.style.display = 'none';
+    }
+}
